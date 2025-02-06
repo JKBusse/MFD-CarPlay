4977af458326f3451ccc4aff5d335006e6267379
JKBusse <48227459+JKBusse@users.noreply.github.com>
2025-02-06 17:37:12 +0100

    Update install.sh to include package updates and modify udev rule setup

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # This is a comment!
+sudo apt-get update -y # Update the package list
+sudo apt-get upgrade -y # Upgrade the installed packages
 sudo mv config.txt /boot/firmware/config.txt # Move the config.txt file to the boot partition
 sudo mv carplay.service /etc/systemd/system/carplay.service # Move the carplay.service file to the systemd directory
 
@@ -8,7 +10,9 @@ mkdir MFD-CarPlay # Create a directory for CarPlay
 cd MFD-CarPlay
 wget https://github.com/rhysmorgan134/react-carplay/releases/download/v4.0.5/react-carplay-4.0.5-arm64.AppImage # Download the latest release of React-CarPlay
 chmod +x react-carplay-4.0.5-arm64.AppImage # Make the AppImage executable
-FILE=/etc/udev/rules.d/52-nodecarplay.rules echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"1314\", ATTR{idProduct}==\"152*\", MODE=\"0660\", GROUP=\"plugdev\"" | sudo tee $FILE # Add a udev rule for the CarPlay dongle
+sudo su # Switch to root user
+echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"1314\", ATTR{idProduct}==\"152*\", MODE=\"0660\", GROUP=\"plugdev\"" > /etc/udev/rules.d/52-nodecarplay.rules # Add a udev rule for the CarPlay dongle
+exit # Exit the root user
 sudo raspi-config nonint do_wayland W1 # Set the display manager to X11
 sudo systemctl restart lightdm.service # Restart the display manager to apply the change to X11
 sudo raspi-config nonint do_vnc 0 -y # Enable VNC
