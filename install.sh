6b9e1ed05b447aebbea1daf432888227c8e19cb6
JKBusse <48227459+JKBusse@users.noreply.github.com>
2025-02-06 08:30:21 +0100

    Add carplay.service and update install.sh for CarPlay setup

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a comment!
-sudo raspi-config nonint do_boot_behaviour B4 # Set the boot behaviour to Desktop with autologin
 sudo mv config.txt /boot/firmware/config.txt # Move the config.txt file to the boot partition
+sudo mv carplay.service /etc/systemd/system/carplay.service # Move the carplay.service file to the systemd directory
 
 cd /usr/local/bin 
 mkdir MFD-CarPlay # Create a directory for CarPlay
@@ -10,3 +10,9 @@ wget https://github.com/rhysmorgan134/react-carplay/releases/download/v4.0.5/rea
 chmod +x react-carplay-4.0.5-arm64.AppImage # Make the AppImage executable
 FILE=/etc/udev/rules.d/52-nodecarplay.rules echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"1314\", ATTR{idProduct}==\"152*\", MODE=\"0660\", GROUP=\"plugdev\"" | sudo tee $FILE # Add a udev rule for the CarPlay dongle
 sudo raspi-config nonint do_wayland W1 # Set the display manager to X11
+sudo raspi-config nonint do_vnc 0 -y # Enable VNC
+sudo raspi-config nonint do_boot_behaviour B4 # Set the boot behaviour to Desktop with autologin
+
+sudo systemctl enable carplay.service # Enable the CarPlay service
+
+sudo reboot # Reboot the Raspberry Pi
