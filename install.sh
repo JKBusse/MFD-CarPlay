95ebf0d626625fa070caa0c806189a28424476cf
JKBusse <48227459+JKBusse@users.noreply.github.com>
2025-02-06 17:04:10 +0100

    Update install.sh to restart the display manager and add a delay before rebooting

--- install.sh
+++ install.sh
@@ -10,9 +10,11 @@ wget https://github.com/rhysmorgan134/react-carplay/releases/download/v4.0.5/rea
 chmod +x react-carplay-4.0.5-arm64.AppImage # Make the AppImage executable
 FILE=/etc/udev/rules.d/52-nodecarplay.rules echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"1314\", ATTR{idProduct}==\"152*\", MODE=\"0660\", GROUP=\"plugdev\"" | sudo tee $FILE # Add a udev rule for the CarPlay dongle
 sudo raspi-config nonint do_wayland W1 # Set the display manager to X11
+sudo systemctl restart lightdm.service # Restart the display manager to apply the change to X11
 sudo raspi-config nonint do_vnc 0 -y # Enable VNC
 sudo raspi-config nonint do_boot_behaviour B4 # Set the boot behaviour to Desktop with autologin
 
 sudo systemctl enable carplay.service # Enable the CarPlay service
 
+sleep 5 # Wait 5 sec. before rebooting
 sudo reboot # Reboot the Raspberry Pi
