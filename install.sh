e24712b24ec20e45e23ed9d13372dfbb968e3ad0
JKBusse <48227459+JKBusse@users.noreply.github.com>
2025-02-06 05:28:19 +0100

    Update install.sh to configure boot behavior and add udev rules for CarPlay

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # This is a comment!
-cd /usr/local/bin
-mkdir MFD-CarPlay
+sudo raspi-config nonint do_boot_behaviour B4 # Set the boot behaviour to Desktop with autologin
+sudo mv config.txt /boot/firmware/config.txt # Move the config.txt file to the boot partition
+
+cd /usr/local/bin 
+mkdir MFD-CarPlay # Create a directory for CarPlay
 cd MFD-CarPlay
-wget https://github.com/rhysmorgan134/react-carplay/releases/download/v4.0.5/react-carplay-4.0.5-arm64.AppImage
-chmod +x react-carplay-4.0.5-arm64.AppImage
+wget https://github.com/rhysmorgan134/react-carplay/releases/download/v4.0.5/react-carplay-4.0.5-arm64.AppImage # Download the latest release of React-CarPlay
+chmod +x react-carplay-4.0.5-arm64.AppImage # Make the AppImage executable
+FILE=/etc/udev/rules.d/52-nodecarplay.rules echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"1314\", ATTR{idProduct}==\"152*\", MODE=\"0660\", GROUP=\"plugdev\"" | sudo tee $FILE # Add a udev rule for the CarPlay dongle
+sudo raspi-config nonint do_wayland W1 # Set the display manager to X11
