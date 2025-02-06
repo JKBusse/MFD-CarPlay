04c700a34fc4da54a65a69bed302f8c9780030e1
JKBusse <48227459+JKBusse@users.noreply.github.com>
2025-02-06 04:38:21 +0100

    c

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # This is a comment!
-git clone https://github.com/rhysmorgan134/react-carplay.git
+cd /usr/local/bin
+mkdir MFD-CarPlay
+cd MFD-CarPlay
+wget https://github.com/rhysmorgan134/react-carplay/releases/download/v4.0.5/react-carplay-4.0.5-arm64.AppImage
+chmod +x react-carplay-4.0.5-arm64.AppImage
