ffa8b1258cc0d1c49531e177b9d0049dd4076280
JKBusse <48227459+JKBusse@users.noreply.github.com>
2025-02-11 03:13:46 +0100

    Added Pinout

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # This is a comment!
 sudo apt-get update -y # Update the package list
 sudo apt-get upgrade -y # Upgrade the installed packages
+sudo apt-get install libxi-dev libx11-dev libxrandr-dev txt2man -y # Install the required packages for the touch calibration tool
 sudo mv config.txt /boot/firmware/config.txt # Move the config.txt file to the boot partition
 sudo mv carplay.service /etc/systemd/system/carplay.service # Move the carplay.service file to the systemd directory
 
@@ -17,6 +18,7 @@ sudo raspi-config nonint do_vnc 0 -y # Enable VNC
 sudo raspi-config nonint do_boot_behaviour B4 # Set the boot behaviour to Desktop with autologin
 
 sudo systemctl enable carplay.service # Enable the CarPlay service
+sudo sh -c "echo -n uvcvideo.quirks=2 >> /boot/firmware/cmdline.txt # Add the uvcvideo quirks for rvc
 
 sleep 5 # Wait 5 sec. before rebooting
 sudo reboot # Reboot the Raspberry Pi
