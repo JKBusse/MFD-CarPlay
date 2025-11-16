e34883eeb7bea2c0d7f67979076e8cc76d90091e
JKBusse <48227459+JKBusse@users.noreply.github.com>
2025-11-16 23:50:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+!/bin/sh
 # This is a comment!
-#sudo apt-get update -y # Update the package list
-#sudo apt-get upgrade -y # Upgrade the installed packages
-#sudo apt-get install libxi-dev libx11-dev libxrandr-dev txt2man -y # Install the required packages for the touch calibration tool
+sudo apt-get update -y # Update the package list
+sudo apt-get upgrade -y # Upgrade the installed packages
+sudo apt-get install libxi-dev libx11-dev libxrandr-dev txt2man -y # Install the required packages for the touch calibration tool
 sudo mv config.txt /boot/firmware/config.txt # Move the config.txt file to the boot partition
 
 cd /home/pi/Desktop # Change to the Desktop directory
@@ -15,10 +15,10 @@ curl -LO https://raw.githubusercontent.com/f-io/pi-carplay/main/setup-pi.sh # Do
 sudo chmod +x setup-pi.sh   # Make the CarPlay setup script executable
 ./setup-pi.sh   # Run the CarPlay setup script
 
-#sudo raspi-config nonint do_wayland W1 # Set the display manager to X11
-#sudo systemctl restart lightdm.service # Restart the display manager to apply the change to X11
-#sudo raspi-config nonint do_vnc 0 -y # Enable VNC
-#sudo raspi-config nonint do_boot_behaviour B4 # Set the boot behaviour to Desktop with autologin
+sudo raspi-config nonint do_wayland W1 # Set the display manager to X11
+sudo systemctl restart lightdm.service # Restart the display manager to apply the change to X11
+sudo raspi-config nonint do_vnc 0 -y # Enable VNC
+sudo raspi-config nonint do_boot_behaviour B4 # Set the boot behaviour to Desktop with autologin
 
 sudo sh -c "echo -n uvcvideo.quirks=2 >> /boot/firmware/cmdline.txt" # Add the uvcvideo quirks for rvc
 
