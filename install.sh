c1ec67e6c0ee3b58d6b31dc4f6093f0f36afdf15
JKBusse <48227459+JKBusse@users.noreply.github.com>
2025-11-16 18:30:55 +0100

    Updated install.dh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a comment!
-sudo apt-get update -y # Update the package list
-sudo apt-get upgrade -y # Upgrade the installed packages
-sudo apt-get install libxi-dev libx11-dev libxrandr-dev txt2man -y # Install the required packages for the touch calibration tool
+#sudo apt-get update -y # Update the package list
+#sudo apt-get upgrade -y # Upgrade the installed packages
+#sudo apt-get install libxi-dev libx11-dev libxrandr-dev txt2man -y # Install the required packages for the touch calibration tool
 sudo mv config.txt /boot/firmware/config.txt # Move the config.txt file to the boot partition
 
-cd /home/pi/desktop # Change to the Desktop directory
+cd /home/pi/Desktop # Change to the Desktop directory
 git clone https://github.com/kreijack/xlibinput_calibrator.git
 cd xlibinput_calibrator/src/
 make
@@ -18,7 +18,7 @@ sudo chmod +x setup-pi.sh   # Make the CarPlay setup script executable
 #sudo raspi-config nonint do_wayland W1 # Set the display manager to X11
 #sudo systemctl restart lightdm.service # Restart the display manager to apply the change to X11
 #sudo raspi-config nonint do_vnc 0 -y # Enable VNC
-sudo raspi-config nonint do_boot_behaviour B4 # Set the boot behaviour to Desktop with autologin
+#sudo raspi-config nonint do_boot_behaviour B4 # Set the boot behaviour to Desktop with autologin
 
 sudo sh -c "echo -n uvcvideo.quirks=2 >> /boot/firmware/cmdline.txt" # Add the uvcvideo quirks for rvc
 
