089636e113f74d1341370a2fde6ce799416391b5
JKBusse <48227459+JKBusse@users.noreply.github.com>
2026-01-27 21:09:27 +0100

    Update install.sh to correct the download URL for the CarPlay setup script

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ cd xlibinput_calibrator/src/
 make
 
 cd /home/pi/Downloads # Change to the Downloads directory
-curl -LO https://raw.githubusercontent.com/f-io/pi-carplay/main/setup-pi.sh # Download the CarPlay setup script
+curl -LO https://raw.githubusercontent.com/f-io/LIVI/refs/heads/main/setup-pi.sh # Download the CarPlay setup script
 sudo chmod +x setup-pi.sh   # Make the CarPlay setup script executable
 ./setup-pi.sh   # Run the CarPlay setup script
 
