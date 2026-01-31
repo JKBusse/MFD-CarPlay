3ab298c7ad52146db800bcba146c40b060e9d214
JKBusse <48227459+JKBusse@users.noreply.github.com>
2026-01-31 20:30:33 +0100

    Add documentation and configuration files for MFD touch retrofit
    - Created disassembly.md for step-by-step disassembly instructions of the MFD for touch retrofit, including images.
    - Added mfdVideoKabel.txt detailing the pinout for the MFD video cable.
    - Introduced config.txt with settings for optional hardware interfaces, audio, camera, and display configurations.
    - Added configNEW.txt with updated HDMI timings and display settings for PAL.
    - Created configOLD.txt with legacy settings for audio, DPI display, and framebuffer configurations.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -y # Update the package list
 sudo apt-get upgrade -y # Upgrade the installed packages
 sudo apt-get install libxi-dev libx11-dev libxrandr-dev txt2man -y # Install the required packages for the touch calibration tool
-sudo mv config.txt /boot/firmware/config.txt # Move the config.txt file to the boot partition
+sudo mv config/config.txt /boot/firmware/config.txt # Move the config.txt file to the boot partition
 
 cd /home/pi/Desktop # Change to the Desktop directory
 git clone https://github.com/kreijack/xlibinput_calibrator.git
@@ -15,12 +15,12 @@ curl -LO https://raw.githubusercontent.com/f-io/LIVI/refs/heads/main/setup-pi.sh
 sudo chmod +x setup-pi.sh   # Make the CarPlay setup script executable
 ./setup-pi.sh   # Run the CarPlay setup script
 
-sudo raspi-config nonint do_wayland W1 # Set the display manager to X11
-sudo systemctl restart lightdm.service # Restart the display manager to apply the change to X11
+#sudo raspi-config nonint do_wayland W1 # Set the display manager to X11
+#sudo systemctl restart lightdm.service # Restart the display manager to apply the change to X11
 sudo raspi-config nonint do_vnc 0 -y # Enable VNC
 sudo raspi-config nonint do_boot_behaviour B4 # Set the boot behaviour to Desktop with autologin
 
-sudo sh -c "echo -n uvcvideo.quirks=2 >> /boot/firmware/cmdline.txt" # Add the uvcvideo quirks for rvc
+#sudo sh -c "echo -n uvcvideo.quirks=2 >> /boot/firmware/cmdline.txt" # Add the uvcvideo quirks for rvc
 
 sleep 5 # Wait 5 sec. before rebooting
 sudo reboot #Reboot the Raspberry Pi
